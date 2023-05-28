8bd5d3c4f97d13464ba0ecf513040fe0ff3c6a98
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-05-28 15:28:49 -0500

    implement server to get fetch the coordinates

--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const ipApiUrl = 'https://api.ipify.org/?format=json';
+const coordApiUrl = 'http://ipwho.is/';
 const ifDataNotAvail = "cat name is not available";
 
 const fetchMyIP = function(callback) {
@@ -27,4 +28,31 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(coordApiUrl + ip, (error, response, body) => {
+
+    const resp = JSON.parse(response.body);
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (resp.success === false) {
+      const msg = `Success status was ${resp.success}. Server message says: ${resp.message} when fetching for IP ${resp.ip}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    const { latitude, longitude } = data;
+
+    if (data.length === 0) {
+      callback(ifDataNotAvail);
+    } else {
+      callback(error, { latitude, longitude });
+    }
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
